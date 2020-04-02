2b91958c36ceb1a0425ee2e8acd7a7854eec7616
keiya01 <keiya.s.0210@gmail.com>
2020-04-02 22:25:40 +0900

    Fixed to support decorator

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,9 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: ['@babel/env', '@babel/typescript'],
-    plugins: ['@babel/proposal-class-properties'],
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      '@babel/proposal-class-properties'
+    ],
   };
 }
