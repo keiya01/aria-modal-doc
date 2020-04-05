f56d5506eb4f4c61c55de9a38228e2e579bcdc8a
keiya01 <keiya.s.0210@gmail.com>
2020-04-05 20:09:02 +0900

    Updated code-area to add prismjs

--- src/components/code-area.ts
+++ src/components/code-area.ts
@@ -6,33 +6,82 @@ import {
   CSSResult,
   property,
 } from 'lit-element';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import Prism from 'prismjs';
 
 class CodeArea extends LitElement {
   @property({ type: String }) code = '';
+  @property({ type: String }) lang = '';
 
   static get styles(): CSSResult {
     return css`
-      div {
+      pre {
+        box-sizing: border-box;
+        width: 100%;
+        overflow-x: scroll;
         background-color: #eee;
         border-radius: 5px;
-        padding: 5px 15px;
+        padding: 0 20px;
       }
       code {
+        color: #333;
         font-size: 1.6rem;
+      }
+      /* common */
+      .token.comment {
+        color: #999;
+      }
+      .token.punctuation {
+        color: #888;
+      }
+      .token.operator {
+        color: #999;
+      }
+      /* JavaScript */
+      .token.string {
+        color: #669900;
+      }
+      .token.keyword {
+        color: #0077aa;
+      }
+      .token.class-name {
+        color: #00aa4a;
+      }
+      .token.function {
+        color: #d49b00;
+      }
+      /* HTML */
+      .token.tag {
+        color: #0077aa;
+      }
+      .token.attr-name {
+        color: #669900;
+      }
+      .token.attr-value {
+        color: #996600;
+      }
+      /* CSS */
+      .token.selector {
+        color: #669900;
+      }
+      .token.property {
+        color: #990055;
       }
     `;
   }
 
   render(): TemplateResult {
-    return html`
-      <div>
-        <pre>${this.multipleCode()}</pre>
-      </div>
-    `;
+    const template = Prism.highlight(
+      this.code,
+      Prism.languages[this.lang],
+      this.lang
+    );
+
+    return html`<pre><code>${unsafeHTML(template)}</code></pre>`;
   }
 
   multipleCode(): TemplateResult[] {
-    return this.code.split('\\n').map((str) => html`<code>${str}</code>`);
+    return this.code.split('\\n').map((str) => html`<span>${str}</span>`);
   }
 }
 
